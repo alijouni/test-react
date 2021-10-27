8cca7b2ab532fb798aceb66a66ddca1f14ffe167
Ali Jouni <alijouni@outlook.de>
2021-10-28 00:05:58 +0300

    Base64

--- src/App.js
+++ src/App.js
@@ -1,35 +1,47 @@
 // import axios from 'axios';
-import React,{Component} from 'react'; 
+import React, { Component } from 'react';
 // import Clarifai from 'clarifai';
 
 // const app = new Clarifai.App({
 //     apiKey: '3b491503921247e8b965300f384d2118',
 // });
 
-
-class App extends Component { 
-
+const initialState = {
+  selectedFile: null,
+  base64Data:""
+}
+class App extends Component {
+ 
     state = { 
   
       // Initially, no file is selected 
       selectedFile: null,
       base64Data:""
-    }; 
+  };
 
-    encodeImageFileAsURL=() => {
-      if (this.state.selectedFile) {
-        
-        var fileReader = new FileReader();
-        fileReader.onload = function(fileLoadedEvent) {
-          var srcData = fileLoadedEvent.target.result; // <--- data: base64
-          this.setState({base64Data:srcData});
-        }.bind(this)
-        fileReader.readAsDataURL(this.state.selectedFile);
-      }
+  
+  encodeImageFileAsURL = () => {
+
+    function getBase64(file, onLoadCallback) {
+      return new Promise(function(resolve, reject) {
+          var reader = new FileReader();
+          reader.onload = function() { resolve(reader.result); };
+          reader.onerror = reject;
+          reader.readAsDataURL(file);
+      });
+  }
+    if (this.state.selectedFile) {
+      var promise = getBase64(this.state.selectedFile);
+      promise.then(result=>this.setState({base64Data:result,selectedFile:null})
+      );
     }
+
+    console.log(this.state.base64Data);
+  }
+
   
     handleApiCall = () => {
-      const raw = JSON.stringify({
+      const raw = {
         "user_app_id": {
           "user_id": "er1pq34lbyad",
           "app_id": "test-react"
@@ -43,28 +55,33 @@ class App extends Component {
             }
           }
         ]
-        });
+      };
       
+      console.log(this.state.base64Data);
         const requestOptions = {
           method: 'POST',
           headers: {
             'Accept': 'application/json',
-            'Authorization': 'Key 3b491503921247e8b965300f384d2118'
+            'Authorization': '3b491503921247e8b965300f384d2118'
           },
           body: raw
         };
         if(this.state.base64Data){
-        fetch("https://api.clarifai.com/v2/models/f76196b43bbd45c99b4f3cd8e8b40a8a/versions/45fb9a671625463fa646c3523a3087d5/outputs", requestOptions)
+        fetch("https://api.clarifai.com/v2/models/f76196b43bbd45c99b4f3cd8e8b40a8a/outputs", requestOptions)
   .then(response => response.text())
-  .then(result => console.log(JSON.parse(result, null, 2).outputs[0].data))
-  .catch(error => console.log('error', error));
+          .then(result => {
+            console.log(JSON.parse(result, null, 2).outputs[0].data);
+            this.state = initialState;
+          })
+            .catch(error => console.log('error', error));
         }
     }
      
     // On file select (from the pop up) 
-    onFileChange = event => { 
+  onFileChange = event => {
       // Update the state 
-      this.setState({ selectedFile: event.target.files[0] }); 
+    this.setState({ selectedFile: event.target.files[0] });
+    //  this.encodeImageFileAsURL();
     }; 
      
     // On file upload (click the upload button) 
@@ -124,14 +141,11 @@ class App extends Component {
               File Upload using React! 
             </h3> 
             <div> 
-                <input type="file" onChange={this.onFileChange} /> 
+                <input type="file" onChange={this.onFileChange ,this.encodeImageFileAsURL} /> 
                 {/* <button onClick={this.onFileUpload}> 
                   Upload! 
                 </button>  */}
-            </div> 
-            {this.encodeImageFileAsURL()}
-            {this.handleApiCall()}
-          {this.fileData()} 
+          </div>
         </div> 
       ); 
     } 
